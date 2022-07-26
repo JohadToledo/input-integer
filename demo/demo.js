f62386e7e68939daf5ea90dfbeb4dd6547ce1c9b
johad <johadtoledo@gmail.com>
2022-07-26 20:21:48 +0200

    add style to the input

--- demo/demo.js
+++ demo/demo.js
@@ -2,6 +2,4 @@ const inputInteger = require('..')
 
 const x = inputInteger()
 
-document.body.innerHTML = `<h1> ${x}</h1>`
-
 document.body.append(x)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 module.exports = inputInteger 
 
+const sheet = new CSSStyleSheet
+const theme = getTheme()
+sheet.replaceSync(theme)
+
 function inputInteger () {
      const el = document.createElement('div')
      const shadow = el.attachShadow({ mode: 'closed' })
      shadow.innerHTML =  `
      <input></input>
      `
+     shadow.adoptedStyleSheets = [sheet]
     return el
+}
+
+function getTheme () {
+    return `
+    input{
+        padding: 6px 8px;
+        background-color: #5da267;
+        border-radius: 8px;
+    }
+    input:focus{
+        background-color: #5d9ba2;
+    }
+    `
 }
