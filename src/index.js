54bbf35c4f32f701c6ee61993b35122898cace05
johad <johadtoledo@gmail.com>
2022-07-30 12:56:15 +0200

    add a fuction for max and min and styling it

--- src/index.js
+++ src/index.js
@@ -9,23 +9,57 @@ function inputInteger () {
      const shadow = el.attachShadow({ mode: 'closed' })
      const input = document.createElement('input')
      input.type = 'number'
+     input.min = 0
+     input.max = 150
+     input.onkeyup = (e) => handleOnkeyup(e, input)
+
+
      shadow.append(input)
-    //  shadow.innerHTML =  `
-    //  <input></input>
-    //  `
+    
      shadow.adoptedStyleSheets = [sheet]
     return el
 }
 
 function getTheme () {
     return `
-    input{
-        padding: 6px 8px;
-        background-color: #5da267;
+    :host {
+        --b: 0, 0%;
+        --color-white: var(--b), 100%;
+        --color-black: var(--b), 0%;
+        --color-grey: var(--b), 85%;
+        --bg-color: var(--color-grey);
+        --shadow-xy: 0 0;
+        --shadow-blur: 8px;
+        --shadow-color: var(--color-white);
+        --shadow-opacity: 0;
+        --shadow-opacity-focus: 0.65;
+      }
+      input {
+        text-align: left;
+        align-items: center;
+        font-size: 1.4rem;
+        font-weight: 200;
+        color: hsla(var(--color-black), 1);
+        background-color: hsla(var(--bg-color), 1);
+        padding: 8px 12px;
+        box-shadow: var(--shadow-xy) var(--shadow-blur) hsla( var(--shadow-color), var(--shadow-opacity));
+        transition: font-size .3s, color .3s, background-color .3s, box-shadow .3s ease-in-out;
+        outline: none;
+        border: 1px solid hsla(var(--bg-color), 1);
         border-radius: 8px;
-    }
-    input:focus{
-        background-color: #5d9ba2;
-    }
+      }
+      input:focus {
+        --shadow-color: var(--color-black);
+        --shadow-opacity: var(--shadow-opacity-focus);
+        --shadow-xy: 4px 4px;
+        box-shadow: var(--shadow-xy) var(--shadow-blur) hsla( var(--shadow-color), var(--shadow-opacity));
+      }
     `
+}
+
+function handleOnkeyup(e, input){
+    console.log(e.target.value)
+    const val = Number(e.target.value)
+    if(val > input.max) input.value = 150
+    else if( val < input.min) input.value = 0
 }
