080ced8a6da3921e139d7a5afea0dccf2e07e7e6
johad <johadtoledo@gmail.com>
2022-08-02 21:58:57 +0200

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,22 @@
 const inputInteger = require('..')
 
-const x = inputInteger()
+const input1 = inputInteger()
+const input2 = inputInteger()
 
-document.body.append(x)
+const title = 'My demo form'
+const subTitle = 'Please fill out the form'
+
+const page = document.createElement('div')
+page.innerHTML = `
+<h1>${title}</h1>
+<h2>${subTitle}</h2>
+<h3>Enter your age</h3>
+<x></x>
+<h3>Enter your year of birth</h3>
+<y></y>
+`
+
+page.querySelector('x').replaceWith(input1)
+page.querySelector('y').replaceWith(input2)
+
+document.body.append(page)
